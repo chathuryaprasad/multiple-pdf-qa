a10eb3a46e5ad8bb315ceda89d77a476edbcb184
CPrasa <chathuryaprasad@gmail.com>
2025-03-19 19:55:38 +0530

    create some streamlit UI components

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+def main():
+    st.set_page_config(page_title = "Chat with your PDFs", page_icon=":books:")
+
+    st.header("Chat with your PDFs :books:")
+    st.text_input("Ask a question about your documents")
+    
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+
+
+
+if __name__ == '__main__':
+    main()
