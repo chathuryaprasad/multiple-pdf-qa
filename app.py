2b1cd995681ff3ef058662b5768ab1ef45ef8762
CPrasa <chathuryaprasad@gmail.com>
2025-03-20 00:47:10 +0530

    impliment multiple pdf read function and test  it

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
+
 
 def main():
+    load_dotenv()
     st.set_page_config(page_title = "Chat with your PDFs", page_icon=":books:")
 
     st.header("Chat with your PDFs :books:")
@@ -8,8 +23,18 @@ def main():
     
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        pdf_docs = st.file_uploader(
+            "Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+             # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
 
+
+            # get the text chunks
+
+            # create vector store
 
 
 if __name__ == '__main__':
