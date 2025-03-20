ff3b25d45f4d8e03abfc54f31e9e58cef0c1b7df
CPrasa <chathuryaprasad@gmail.com>
2025-03-20 20:12:10 +0530

    split the text in to the chunks

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
-
+from langchain.text_splitter import CharacterTextSplitter
 
 
 def get_pdf_text(pdf_docs):
@@ -12,6 +12,16 @@ def get_pdf_text(pdf_docs):
             text += page.extract_text()
 
     return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
 
 
 def main():
@@ -29,12 +39,14 @@ def main():
             with st.spinner("Processing"):
              # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
-                st.write(raw_text)
+                #st.write(raw_text)
 
 
             # get the text chunks
-
+                text_chunks = get_text_chunks(raw_text)
+                st.write(text_chunks)
             # create vector store
+            
 
 
 if __name__ == '__main__':
