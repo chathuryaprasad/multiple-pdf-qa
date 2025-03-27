ad01b8e24233ac318382990b41a2ba4d4a454bc1
CPrasa <chathuryaprasad@gmail.com>
2025-03-27 23:48:32 +0530

    add session state and connect response correctly

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from sentence_transformers import SentenceTransformer
 from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -45,16 +47,36 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
+def handle_userinput(user_question):
+    response = st.session_state.conversation{{'question': user_question}}
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i & 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
+
 def main():
     load_dotenv()
     st.set_page_config(page_title = "Chat with your PDFs", page_icon=":books:")
 
+    st.write(css, unsafe_allow_html=True)
+
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
 
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with your PDFs :books:")
-    st.text_input("Ask a question about your documents")
-    
+    user_question = st.text_input("Ask a question about your documents")
+    if user_question:
+        handle_userinput(user_question)
+
+
     with st.sidebar:
         st.subheader("Your documents")
         pdf_docs = st.file_uploader(
